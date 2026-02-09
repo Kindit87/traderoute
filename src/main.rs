ccf9de223b9ed4462d952582dba9fe9e076d0656
kindit <kindit@proton.me>
2026-02-09 15:33:41 +0500

    fix: Change user_agent and header

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let url = "https://inara.cz/elite/market-traderoutes/?ps1=Kamadhenu&pi10=19320&pi2=1000&pi5=8&pi3=3&pi9=0&pi4=0&pi7=50000&pi12=50000&pi14=0&pi15=0&pi1=0";
 
     let client = Client::builder()
-        .user_agent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-             AppleWebKit/537.36 (KHTML, like Gecko) \
-             Chrome/121.0.0.0 Safari/537.36",
-        )
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/147.0")
         .build()?;
 
     let response = client
         .get(url)
-        .header(ACCEPT, "text/html,application/xhtml+xml")
+        .header(
+            ACCEPT,
+            "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        )
         .header(ACCEPT_LANGUAGE, "en-US,en;q=0.9")
         .header(REFERER, "https://google.com")
         .send()?
