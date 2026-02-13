ceb440564f8a7d4ced57904bb32c56bfb55c88e4
kindit <kindit@proton.me>
2026-02-13 17:29:05 +0500

    fix: Should be better

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use reqwest::blocking::Client;
-use reqwest::header::{ACCEPT, ACCEPT_LANGUAGE, REFERER};
+use reqwest::header::{ACCEPT, ACCEPT_LANGUAGE, CONNECTION, HOST, REFERER};
 use scraper::{Html, Selector};
 use std::error::Error;
+use ua_generator::ua::spoof_firefox_linux_ua;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let url = "https://inara.cz/elite/market-traderoutes/?ps1=Kamadhenu&pi10=19320&pi2=1000&pi5=8&pi3=3&pi9=0&pi4=0&pi7=50000&pi12=50000&pi14=0&pi15=0&pi1=0";
 
-    let client = Client::builder()
-        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/147.0")
-        .build()?;
+    let user_agent = spoof_firefox_linux_ua();
+    let client = Client::builder().user_agent(user_agent).build()?;
 
     let response = client
         .get(url)
-        .header(
-            ACCEPT,
-            "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        )
+        .header(ACCEPT, "*/*")
         .header(ACCEPT_LANGUAGE, "en-US,en;q=0.9")
-        .header(REFERER, "https://google.com")
+        .header(REFERER, url)
+        .header(CONNECTION, "keep-alive")
+        .header(HOST, "inara.cz")
         .send()?
         .text()?;
 
@@ -34,6 +33,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .unwrap()
                 .trim()
         );
+    } else {
+        panic!("Parsing error");
     }
 
     Ok(())
